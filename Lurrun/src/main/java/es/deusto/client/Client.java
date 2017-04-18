b16bd41edeab2001df5b04b83bde0152f3a478f3
Ainhoa <ainhoa.achon@opendeusto.es>
2017-04-18 13:17:29 +0200

    Updated the project to a working version and stablished a viable connection between the server and the client

---
+++ Lurrun/src/main/java/es/deusto/client/Client.java
@@ -0,0 +1,33 @@
+package es.deusto.client;
+
+
+import es.deusto.server.IServer;
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+		if (args.length != 3) {
+			System.out.println("Use: java [policy] [codebase] Client.Client [host] [port] [server]");
+			System.exit(0);
+		}
+		System.out.println("[C] Client here =)");
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new SecurityManager());
+		}
+
+		try {
+			String name = "//" + args[0] + ":" + args[1] + "/" + args[2];
+			IServer objHello = (IServer) java.rmi.Naming.lookup(name);
+			// Register to be allowed to send messages
+			//objHello.registerUser("dipina", "dipina");
+			//System.out.println("* Message coming from the server: '" + objHello.sayMessage("dipina", "dipina", "This is a test!") + "'");
+			System.out.println("[C] Sending salutations");
+			System.out.println(objHello.sayHello());
+			
+		} catch (Exception e) {
+			System.err.println("RMI Example exception: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+}
---
+++ Lurrun/src/main/java/es/deusto/server/IServer.java
@@ -0,0 +1,12 @@
+package es.deusto.server;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface IServer extends Remote {
+	
+//	String sayMessage(String login, String password, String message) throws RemoteException;
+//	void registerUser(String login, String password) throws RemoteException;
+	String sayHello() throws RemoteException;
+
+}
